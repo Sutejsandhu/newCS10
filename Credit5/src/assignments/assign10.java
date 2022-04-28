753df79f80c2812eb3d671e5661a66609c5349f1
36226002 <36226002@L815WD220030.edu.cbe.ab.ca>
2022-04-28 15:02:14 -0600

    Assignment 10

---
+++ Credit5/src/assignments/assign10.java
@@ -0,0 +1,50 @@
+/*
+
+Program: assign10.java          Date: April 28, 2022
+
+
+Author: Sutej Sandhu 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+
+package assignments;
+
+import java.util.Scanner;
+
+
+public class assign10 
+{
+ public static void main(String args[])
+    {
+       Scanner userinput = new Scanner(System.in);
+       System.out.println("Enter an integer: ");
+       int digit = userinput.nextInt();
+       if (digit % 2 == 0) {
+           System.out.println("Your integer " +digit+ " is even!");
+                   } else {
+           System.out.println("Your integer " +digit+ " is odd");
+       }
+       
+    }
+
+}
+
+/* Screen Dump
+ 
+Enter an integer: 
+12
+Your integer 12 is even!
+
+Enter an integer: 
+5125
+Your integer 5125 is odd
+ 
+Enter an integer: 
+-123451591
+Your integer -123451591 is odd
+
+ */
